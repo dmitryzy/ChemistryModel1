78c581fe716118c64a9a3884de63dddeb19a2605
Dmitry Zymai <dmitry-zy@yandex.ru>
2016-03-28 03:30:28 +0300

    Добавлена проверка наличия в базе данных

--- ChemistryModel/DataBase.h
+++ ChemistryModel/DataBase.h
@@ -11,6 +11,7 @@ public:
 	DataBase(std::string dbname, std::string username, std::string dbpassw);
 	SubstanceParametrs GetSubstData(std::string SubstFormula);
 	void SetSubstData(SubstanceParametrs Param);
+	bool in_database(std::string Formula);
 	~DataBase();
 private:
 	std::string DBname;
